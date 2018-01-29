d213719e638560078740a174ac6a39257e476498
Rajesh77 <rajesh.business7@gmail.com>
2018-01-29 15:00:01 +0530

    no modifications done

---
+++ src/Get2.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Get2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList x=new ArrayList();
+		x.add(20);
+		x.add(50);
+		x.add(56);
+		int y=(int) x.get(1);
+		System.out.println(y);
+		
+	}
+
+}
